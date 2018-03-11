7acada6671808bf431db33ea96c99faa07482801
Unknown <neo_cloud@hotmail.com>
2018-03-11 11:44:35 +0100

    Setup watch for typescript

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ const sourcemaps = require('gulp-sourcemaps');
 const runSequence = require('run-sequence');
 const del = require('del');
 
+const tsProject = typescript.createProject('tsconfig.json');
+
 gulp.task('clean', function () {
     return del('target')
 });
@@ -14,12 +16,13 @@ gulp.task('build', function (done) {
 });
 
 gulp.task('build:typescript', function () {
-    var project = typescript.createProject('tsconfig.json');
-    return project.src()
+    return gulp.src('src/**/*.ts')
         .pipe(tslint())
-        .pipe(tslint.report())
+        .pipe(tslint.report({
+            emitError: false
+        }))
         .pipe(sourcemaps.init())
-        .pipe(project()).js
+        .pipe(tsProject()).js
         .pipe(sourcemaps.write('.', { includeContent: false, sourceRoot: '../src' }))
         .pipe(gulp.dest("target"));
 });
@@ -37,4 +40,8 @@ gulp.task('build:lib', function(){
 
 gulp.task('build:clean', function () {
     return del('target')
+});
+
+gulp.task('watch', ['build'], function(){
+    gulp.watch('src/**/*.ts', ['build:typescript']);
 });

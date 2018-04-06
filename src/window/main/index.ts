400fbda93f9f5bbf0acecb00371130216a0291a7
Unknown <neo_cloud@hotmail.com>
2018-04-06 20:22:25 +0200

    Reset repo, setup angular

--- src/window/main/index.ts
+++
@@ -1,6 +0,0 @@
-import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
-
-import { AppModule } from "./module/app.module";
-
-const platform = platformBrowserDynamic();
-platform.bootstrapModule(AppModule);
